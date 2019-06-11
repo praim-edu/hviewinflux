36369aef3c97bf824cb054bade8e36a9ff9d568d
jacopoferraro <jacopoferraro@praim.com>
2019-06-11 12:25:43 +0200

    Added cpu, ram, diskusage and diskio to influx

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const client = new Influx('http://10.2.138.212:8086/mydb')
 const axios = require('axios')
 
 const hvServerName = "hview77.sup.praim.com"
-const hvServerUrl = "http://" + hvServerName + ":8000/SessionLocalSummaryView/"
+const hvServerUrl = "http://" + hvServerName + ":8000/"
+const hvServerCPU = "cpu"
+const hvServerRAM = "ram"
+const hvServerIO = "diskio"
+const hvServerMB = "diskusage"
+const hvServerVM = "SessionLocalSummaryView/"
 const rp = 'test-rp';
 
 // i --> integer
@@ -29,26 +34,26 @@ const tagSchema = {
     app: ['hviewinflux'],
 };
 
-function sendDataToInflux(){
+function sendHVDataToInflux() {
     client.createDatabase().then(() => client.createRetentionPolicy(rp, '2h')).then(() => {
         let namesData
         // make a get request to the url
         axios({
             method: 'get',
-            url: hvServerUrl + "namesdata",
+            url: hvServerUrl + hvServerVM + "namesdata",
             headers: { 'Accept': 'application/json' }, // this api needs this header set for the request
         }).then(res => {
             namesData = res.data;
             axios({
                 method: 'get',
-                url: hvServerUrl + "sessiondata",
+                url: hvServerUrl + hvServerVM + "sessiondata",
                 headers: { 'Accept': 'application/json' },
             }).then(res => {
                 sessionData = res.data;
                 if (sessionData.length == namesData.length) {
                     for (let i = 0; i < sessionData.length; i++) {
                         client.write(hvServerName)
-                            .tag({app: ['hviewinflux']})
+                            .tag({ app: ['hviewinflux'] })
                             .field({
                                 // from namesdata
                                 "machineorrdsservername": namesData[i].MachineOrRDSServerName,
@@ -65,13 +70,82 @@ function sendDataToInflux(){
                                 "disconnecttime": sessionData[i].DisconnectTime
                             })
                             .set({ RP: rp })
-                            .then(() => { console.info('write point success') })
+                            .then(() => { console.info('write point success(serverInformation)') })
                             .catch(err => { console.error(err) });
-                        }
                     }
+                }
             }).catch(err => { console.error(err) })
-        }).catch(err => { console.error(log) })
-    }).catch(err => { console.error(log) })
+        }).catch(err => { console.error(err) })
+    }).catch(err => { console.error(err) })
 }
 
-setInterval(sendDataToInflux, 600*1000);
+function sendServerHealthToInflux() {
+    client.createDatabase().then(() => client.createRetentionPolicy(rp, '2h')).then(() => {
+        let cpu
+        let ram
+        let io
+        let mb
+        // make a get request to the url
+        axios({
+            method: 'get',
+            url: hvServerUrl + hvServerCPU,
+            headers: { 'Accept': 'application/json' },
+        }).then(res => {
+            cpu = res.data;
+            axios({
+                method: 'get',
+                url: hvServerUrl + hvServerRAM,
+                headers: { 'Accept': 'application/json' },
+            }).then(res => {
+                ram = res.data;
+                axios({
+                    method: 'get',
+                    url: hvServerUrl + hvServerMB,
+                    headers: { 'Accept': 'application/json' },
+                }).then(res => {
+                    mb = res.data;
+                    client.write(hvServerName)
+                        .tag({ app: ['hviewinflux'] })
+                        .field({
+                            "cpu": cpu,
+                            "ram": ram,
+                            "mb": mb
+                        })
+                        .set({ RP: rp })
+                        .then(() => { console.info('write point success(cpu_ram_mb)') })
+                        .catch(err => { console.error(err) });
+                }).catch(err => { console.error(err) })
+            }).catch(err => { console.error(err) })
+        }).catch(err => { console.error(err) })
+    }).catch(err => { console.error(err) })
+}
+function sendServerDiskIOToInflux() {
+    client.createDatabase().then(() => client.createRetentionPolicy(rp, '2h')).then(() => {
+        let io
+
+        // make a get request to the url
+        axios({
+            method: 'get',
+            url: hvServerUrl + hvServerIO,
+            headers: { 'Accept': 'application/json' },
+        }).then(res => {
+            client.write(hvServerName)
+                .tag({ app: ['hviewinflux'] })
+                .field({
+                    "io": res.data,
+                })
+                .set({ RP: rp })
+                .then(() => { console.info('write point success(io)') })
+                .catch(err => { console.error(err) });
+        }).catch(err => { console.error(err) })
+    }).catch(err => { console.error(err) })
+}
+// horizon view data every 10 minutes
+setInterval(sendHVDataToInflux, 600 * 1000);
+
+// server health every 1 minute
+setInterval(sendServerHealthToInflux, 60 * 1000);
+
+// server health every 5 seconds
+setInterval(sendServerDiskIOToInflux, 5 * 1000);
+
